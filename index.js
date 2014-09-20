45b916441d6c45924c8a5a03b846ff60aae8f284
Joseph Dykstra <josephdykstra@gmail.com>
2014-09-20 16:20:03 -0500

    Debugging feast your eyes :D

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ module.exports = function ttl(db, opts) {
 	} else if (!db.sublevel) {
 		throw new Error('You must pass a level-sublevel ready database')
 	}
-	opts = xtend({ttl: 3600000, checkInterval: 10000}, opts) //, refreshOnGet: false
+	opts = xtend({ttl: 3600000, checkInterval: 10000}, opts)
+	console.dir(opts)
 	var expirer = new Expirer(opts.ttl, db.sublevel('expirer'), opts.checkInterval)
-	db.on('put', expirer.touch.bind(expirer)) //I hope this is not recursive; touch() calls put() in a subDb
-	//opts.refreshOnGet && db.on('get', expirer.touch.bind(expirer)) //leveldb does not emit 'get' events. :(
-	expirer.on('expire', db.del.bind(db)) //if err, this will probably throw
+	//db.on('put', expirer.touch.bind(expirer))
+	//expirer.on('expire', db.del.bind(db))
+	db.on('put', function (key) {
+		console.log('touching ' + key)
+		expirer.touch(key)
+	})
+	expirer.on('expire', function (key) {
+		console.log('deleting ' + key)
+		db.del(key)
+	})
 }
