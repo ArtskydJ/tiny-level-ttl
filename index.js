5d79e6be0a33b72d646a1618d4c33f64f91ad00b
Joseph Dykstra <josephdykstra@gmail.com>
2014-09-25 21:08:09 -0500

    1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Expirer = require('expire-unused-keys')
 var xtend = require('xtend')
+var hooks = require('level-hooks')
+var lock = require('level-lock')
 
 module.exports = function ttl(db, opts) {
 	if (!db) {
@@ -7,19 +9,25 @@ module.exports = function ttl(db, opts) {
 	} else if (!db.sublevel) {
 		throw new Error('You must pass a level-sublevel ready database')
 	}
-	opts = xtend({ttl: 3600000, checkInterval: 10000, refreshOnGet: false}, opts)
+	hooks(db)
+	opts = xtend({ttl: 3600000, checkInterval: 10000}, opts)
 	var expirer = new Expirer(opts.ttl, db.sublevel('expirer'), opts.checkInterval)
-	var put = db.put
-	db.put = function (key) {
-		expirer.touch(key)
-		put.apply(db, arguments)
-	}
-	if (opts.refreshOnGet) {
-		var get = db.get
-		db.get = function (key) {
-			expirer.touch(key)
-			get.apply(db, arguments)
+	db.hooks.post(function (change) {
+		if (change.type === 'put') {
+			expirer.touch(change.key)
+		} else if (change.type === 'del') {
+			expirer.forget(change.key)
 		}
-	}
-	expirer.on('expire', db.del.bind(db)) //if err, this will probably throw
+	})
+	expirer.on('expire', function (key) {
+		var unlock = lock(db, key, 'w')
+		if (unlock) {
+			db.del(key, function (err) {
+				unlock()
+			})
+		} else {
+			expirer.touch(key) //retry next time
+		}
+	}) //if err, this will probably throw
+	db.refreshTtl = expirer.touch
 }
