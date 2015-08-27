3ae93152ff04caa76fa1e40b1be008736b56a0af
Joseph Dykstra <josephdykstra@gmail.com>
2015-08-26 21:59:29 -0500

    Bump timeouts due to failing tests

--- test/testLocking.js
+++ test/testLocking.js
@@ -12,26 +12,26 @@ test('basic locking', function (t) {
 	db.put('hi', 'wuzzup')
 	var unlock = undefined
 	db.on('error', t.fail.bind(t))
-	setTimeout(function () { //before ttl
+	setTimeout(function () { // before ttl
 		unlock = lock(db, 'hi', 'w')
 		t.ok(unlock, 'got lock')
 	}, 70)
-	setTimeout(function () { //after ttl
+	setTimeout(function () { // after ttl
 		unlock()
 		db.get('hi', function (err, value) {
 			t.notOk(err, 'no error')
 			t.notOk(err && err.notFound, 'no "notFound" error')
-			t.ok(value, 'key exists, idk if that is good')
+			t.ok(value, 'key exists')
 		})
 	}, 130)
 	setTimeout(function () {
-		db.get('hi', function (err, value) { //does it retry?
+		db.get('hi', function (err, value) { // does it retry?
 			t.ok(err, 'error')
 			t.ok(err && err.notFound, '"notFound" error')
 			t.notOk(value, 'key does not exist')
 		})
 		t.end()
-	}, 220)
+	}, 250)
 })
 
 test('more locking and deletions', function (t) {
@@ -43,15 +43,15 @@ test('more locking and deletions', function (t) {
 	db.put('hi', 'wuzzup')
 	var unlock = undefined
 	db.on('error', t.fail.bind(t))
-	setTimeout(function () { //before ttl
+	setTimeout(function () { // before ttl
 		unlock = lock(db, 'hi', 'w')
 		t.ok(unlock, 'got lock')
 	}, 70)
-	setTimeout(function () { //after ttl
+	setTimeout(function () { // after ttl
 		db.del('hi', function (err) {
 			t.notOk(err, 'no error on deletion')
 			unlock()
 		})
 	}, 130)
-	setTimeout(t.end.bind(t), 220)
+	setTimeout(t.end.bind(t), 250)
 })
